6ab461b7e34e3ec4bb341d4fade6c06ca8f3054d
awa-manju <awa.manju@gmail.com>
2016-07-17 23:37:41 +0900

    mod

--- encode.py
+++ encode.py
@@ -28,5 +28,5 @@ def encode():
 
 
 if __name__ == '__main__':
-    # comskip()
+    comskip()
     encode()
--- libs/encoder.py
+++ libs/encoder.py
@@ -25,13 +25,20 @@ class Encoder(Base):
     def execute(self, filename):
         basefilename, _ = os.path.splitext(filename)
         ext = '.mp4'
-        encoded_file = os.path.join(self.dest_dir, os.path.basename(basefilename) + ext)
+        encoded_basefile = basefilename + ext
         cmd_opt_input = self._create_cmd_opt({'i': filename})
         cmd_opt_vsize = self._create_cmd_opt({'s': '1280x720'})
         arg_list = []
         arg_list.extend(cmd_opt_input)
         arg_list.extend(cmd_opt_vsize)
-        arg_list.extend([basefilename + ext])
+        arg_list.extend([encoded_basefile])
         self._execute_cmd(self.cmd, arg_list)
-        shutil.move(filename, encoded_file)
-        copy_timestamp(encoded_file, afterfile=encoded_file)
+        copy_timestamp(filename, afterfile=encoded_basefile)
+        encoded_srcfile = os.path.join(
+                os.path.dirname(filename), os.path.basename(basefilename) + ext)
+        encoded_dstfile = os.path.join(
+                self.dest_dir, os.path.basename(basefilename) + ext)
+        try:
+            shutil.move(encoded_srcfile, encoded_dstfile)
+        except OSError:
+            pass
