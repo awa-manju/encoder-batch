96fea3aa18968427097fd4500cd59037d992b5b0
awa-manju <awa.manju@gmail.com>
2016-09-03 11:10:46 +0900

    mod

--- age_backup.sh
+++ age_backup.sh
@@ -8,5 +8,5 @@ TS_DIR=/mnt/archive/ts/backup
 list_aged_files $TS_DIR *.m2ts* $TS_AGE_DAYS | xargs rm -f
 
 MP4_AGE_DAYS=3
-MP4_TS_DIR=/mnt/share/videos
-list_aged_files $MP4_DIR *mp4 $MP4_AGE_DAYS | xargs rm -f
+MP4_DIR=/mnt/share/videos
+list_aged_files $MP4_DIR *.mp4 $MP4_AGE_DAYS | xargs rm -f
